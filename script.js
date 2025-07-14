d09953189aaa554dfd9b5d5748f7db78c2b4289d
vinod24256 <184947573+vinod24256@users.noreply.github.com>
2025-07-14 13:22:11 +0530

    high score
    added high score feauture

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +29,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
     // guess too high
   } else if (guess > secretNumber) {
     if (score > 1) {
